f0f5c0339ef63e84656af8d54d235a38a5f792cb
Adam Pusio <adam.pusio@outlook.com>
2022-05-23 18:48:18 +0200

    Initial commit

---
+++ src/main/java/com/sda/zdjavapol107/App.java
@@ -0,0 +1,13 @@
+package com.sda.zdjavapol107;
+
+import java.util.List;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        List.of("", "a").stream();
+    }
+}
