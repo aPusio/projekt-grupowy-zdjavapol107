6d569c6331b0e45edf21a9192ba14564f942beb1
Adam Pusio <adam.pusio@outlook.com>
2022-05-24 20:48:23 +0200

    1-stworzenie-menu - stworzenie menu i dodanie podstawowych zaleznosci

--- src/main/java/com/sda/zdjavapol107/App.java
+++ src/main/java/com/sda/zdjavapol107/App.java
@@ -1,15 +1,40 @@
 package com.sda.zdjavapol107;
 
-import java.util.List;
+import org.hibernate.SessionFactory;
 
-/**
- * Hello world!
- */
+import java.util.Scanner;
+
+
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-        List.of("", "a").stream();
-//        sddsdfsdf
-//          dasdasdad
+        SessionFactory sessionFactory = new HibernateFactory().getSessionFactory();
+        System.out.println("HELLO!");
+        System.out.println("1. JAKAS GRA");
+        System.out.println("2. JAKAS GRA");
+        System.out.println("3. JAKAS GRA");
+        System.out.println("4. JAKAS GRA");
+        System.out.println("PODAJ NUMER GRY W KTORA CHCESZ GRAC :)");
+
+        Scanner scanner = new Scanner(System.in);
+        //TODO tutaj powinna byc pewnie jakas petla
+        int gameNumber = scanner.nextInt();
+
+        switch (gameNumber){
+            case 1:
+                System.out.println("HERE SHOULD BE YOUR GAME ! ");
+                break;
+            case 2:
+                System.out.println("HERE SHOULD BE YOUR GAME ! ");
+                break;
+            case 3:
+                System.out.println("HERE SHOULD BE YOUR GAME ! ");
+                break;
+            case 4:
+                System.out.println("HERE SHOULD BE YOUR GAME ! ");
+                break;
+            default:
+                System.out.println("WHAT ?!");
+        }
+        sessionFactory.close();
     }
 }
