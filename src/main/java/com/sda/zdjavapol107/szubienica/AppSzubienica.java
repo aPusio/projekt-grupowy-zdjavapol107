7891733eaed7a834de37ad13822869f6f42d2433
14P89 <przem14k@gmail.com>
2022-05-26 09:17:31 +0200

    7 szubienica - dodanie menu w grze

--- src/main/java/com/sda/zdjavapol107/szubienica/AppSzubienica.java
+++ src/main/java/com/sda/zdjavapol107/szubienica/AppSzubienica.java
@@ -1,7 +1,54 @@
 package com.sda.zdjavapol107.szubienica;
 
+import java.util.Scanner;
+
 public class AppSzubienica {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+
+        String szubienica = "      _________\n" +
+                "     |/        |\n" +
+                "     |         |\n" +
+                "     |         |\n" +
+                "     |\n" +
+                "     |       (x_x)\n" +
+                "     |        /|\\\n" +
+                "     |         |\n" +
+                " ____|____    / \\" +
+                "__SZUBIENICA__\n";
+
+        String next;
+        boolean exit = false;
+
+        while (!exit) {
+            System.out.println(szubienica);
+
+            System.out.println();
+            System.out.println("1. NOWA GRA");
+            System.out.println("2. WCZYTAJ GRE");
+            System.out.println("3. RANKING GRACZY");
+            System.out.println("4. WYJDZ Z GRY");
+
+            next = scanner.next();
+
+            switch (next) {
+                case "1" -> {
+                    System.out.println("nowa gra");
+                }
+                case "2" -> {
+                    System.out.println("wczytaj gre");
+                }
+                case "3" -> {
+                    System.out.println("ranking graczy");
+                }
+                case "4" -> {
+                    exit = true;
+                }
+                default -> {
+                    System.out.println("Wybierz ponownie.");
+                }
+            }
+        }
     }
 }
