fb0c2039b54e868f1a97afe92c2eef891329bce0
rakubwa <kwdowik@yahoo.com>
2022-05-26 18:08:24 +0200

    6-memorki, stworzyłem 3 klasy - Game, Point i User.

---
+++ src/main/java/com/sda/zdjavapol107/Memorki/Point.java
@@ -0,0 +1,7 @@
+package com.sda.zdjavapol107.memorki;
+//generate hashcode and equals
+
+public class Point {
+
+
+}
---
+++ src/main/java/com/sda/zdjavapol107/Memorki/User.java
@@ -0,0 +1,22 @@
+package com.sda.zdjavapol107.memorki;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class User {
+    public static void main(String[] args) {
+        ArrayList<String> users = new ArrayList();
+        Scanner sc = new Scanner(System.in);
+        boolean isRunning = true;
+
+
+        System.out.println("Please enter your name.");
+        String name = sc.nextLine();
+        users.add(name);
+
+//        map with point, x y
+
+
+    }
+
+}
